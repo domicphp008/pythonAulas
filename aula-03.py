6742ac53d243416046ae95d31761cbfa67a47466
Domicio Jose <testedomicio@hotmail.com>
2015-12-26 21:13:47 -0200

    Delimitadores
    (quotes) em String com Pyton

---
+++ aula-03.py
@@ -0,0 +1,28 @@
+Python 3.4.4rc1 (v3.4.4rc1:04f3f725896c, Dec  6 2015, 16:42:12) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x ='casa'
+>>> y = "casa"
+>>> x = """ Estas string são muito usada para fazer comentários no pyhon.
+"""
+>>> 
+>>> 
+>>> x
+' Estas string são muito usada para fazer comentários no pyhon.\n'
+>>> print x
+SyntaxError: Missing parentheses in call to 'print'
+>>> print(x)
+ Estas string são muito usada para fazer comentários no pyhon.
+
+>>> """ nesta exemplo acima mostra um string formatada """
+' nesta exemplo acima mostra um string formatada '
+>>> 
+>>> 'let's go'
+SyntaxError: invalid syntax
+>>> 
+>>> "let's go"
+"let's go"
+>>> 'lest\'s go'
+"lest's go"
+>>> "let\"s go"
+'let"s go'
+>>> 
