0f4f57a18062f087847052e289e462246e2a84ee
Domicio Jose <testedomicio@hotmail.com>
2015-12-26 22:00:59 -0200

    Operadores aritiméticos e divisão no Python 2 e Python 3

---
+++ aula-06.py
@@ -0,0 +1,31 @@
+Python 3.4.4rc1 (v3.4.4rc1:04f3f725896c, Dec  6 2015, 16:42:12) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 2 +3
+5
+>>> 2 - 4
+-2
+>>> 2 * 5
+10
+>>> 2 ** 4
+16
+>>> 2 % 4
+2
+>>> 2 / 4
+0.5
+>>> 2 / float(4)
+0.5
+>>> from __future__import division
+SyntaxError: invalid syntax
+>>> 
+>>> from __future__ import division
+>>> 
+>>> 
+>>> 2 /float (4)
+0.5
+>>> from __future__ import division
+>>> 
+>>> 2/4
+0.5
+>>> 2+ 3j
+(2+3j)
+>>> 
