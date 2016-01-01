dc206f25b3c07d94e4ad5204aaa513c59a81bfa9
Domicio Jose <testedomicio@hotmail.com>
2016-01-01 12:44:52 -0200

    5 - Python - Variáveis, Tipos de Dados e o comando Type
    https://www.youtube.com/watch?v=SYioCdLPmfw&index=5&list=PLucm8g_ezqNrrtduPx7s4BM8phepMn9I2

---
+++ bosontreinamentos05.py
@@ -0,0 +1,35 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x= 6
+>>> x
+6
+>>> y = True
+>>> y
+True
+>>> w = 5,0
+>>> w
+(5, 0)
+>>> z = "Domicio"
+>>> z
+'Domicio'
+>>> print(type(x))
+<class 'int'>
+>>> print(type(y))
+<class 'bool'>
+>>> print(type(w))
+<class 'tuple'>
+>>> print(type(z))
+<class 'str'>
+>>> k = 0
+>>> k
+0
+>>> m = 0
+>>> m
+0
+>>> m = 6
+>>> m
+6
+>>> k = m + x
+>>> k
+12
+>>> 
