5434bd7b01fa4923c0eb452efeddaa3f1ce0a4c4
Domicio Jose <testedomicio@hotmail.com>
2016-01-02 16:35:44 -0200

    esta programação da sem erros

---
+++ bosontreinamentos07a.py
@@ -0,0 +1,34 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a = 5
+>>> b = 4
+>>> print(a)
+5
+>>> print(b)
+4
+>>> print (a + b)
+9
+>>> print(a - b)
+1
+>>> print(a * b)
+20
+>>> print(a / b)
+1.25
+>>> print(a ** b)
+625
+>>> c = a + b
+>>> print (c)
+9
+>>> c = a + a * b
+>>> print(c)
+25
+>>> c = (a + a) * b
+>>> print(c)
+40
+>>> a= 1
+>>> b = 2
+>>> c = 3
+>>> d = b ** b - 4 * a *c
+>>> print(d)
+-8
+>>> 
