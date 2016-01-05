8938d927fffa65afe8244aaa7131c82897e4d92f
Domicio Jose <testedomicio@hotmail.com>
2016-01-05 19:30:38 -0200

    FisicaInterativaAula1Python
    Link: https://www.youtube.com/watch?v=e6bFmit6WpQ

---
+++ ResolvendoEquaçãoDo2GrauAula1.py
@@ -0,0 +1,22 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 2+5
+7
+>>> 40/8
+5.0
+>>> a =1
+>>> b = -2
+>>> c = -3
+>>> delta = b **2 -4 * a *c
+>>> print(delta)
+16
+>>> raizdelta = delta **0.5
+>>> print(raizdelta)
+4.0
+>>> x1 = ( -b + raizdelta) / (2 *a)
+>>> x2 = ( -b - raizdelta) / (2 * a)
+>>> print(x1)
+3.0
+>>> print (x2)
+-1.0
+>>> 
